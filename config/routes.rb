aa219e5d37a1402e0a427b430656e7eb43351bcc
liannsoo <asoosai@uchicago.edu>
2024-11-05 15:47:26 +0000

    2nd

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
   get("/", { :controller => "misc", :action => "homepage" })
+  get("/directors/youngest", { :controller => "directors", :action => "youngest" })
+  get("/directors/eldest", { :controller => "directors", :action => "eldest" })
+  get("/directors/:the_id", { :controller => "directors", :action => "show" })
 end
